b10003ca09e99e4e3fd0a45bbbc7caecee1fec37
VilMe <vilma.medrano00@gmail.com>
2026-01-02 23:12:43 -0600

    define get rates function

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ import os
 # Contstants
 BASE_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
 API_KEY: Final[str] = os.environ['EXCHANGE_RATE_API_KEY']
+
+
+
+def get_rates(mock: bool = False) -> dict:
+    if mock:
+        with open('rates.json', 'r') as file:
+            return json.load(file)
+        
+    payload: dict = {'access_key': API_KEY}
+    request = requests.ge(url=BASE_URL, params=payload)
+    data: dict = request.json()
+
+    with open('rates.json', 'w') as file:
+        json.dump(data, file)
+    
+
+    return data
+
+
+get_rates()
