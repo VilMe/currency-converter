b56edc21c1e37046598d556ef9886e17efad1d17
VilMe <vilma.medrano00@gmail.com>
2026-01-02 22:55:12 -0600

    update gitignore and define base url variable and API key

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 from typing import Final
 import requests
+import os
 
 
 
 # Contstants
-BASE_URL: Final[str] = ''
-API_KEY: Final[str] = ''
+BASE_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
+API_KEY: Final[str] = os.environ['EXCHANGE_RATE_API_KEY']
