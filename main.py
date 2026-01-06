c8e9702e9c6ec28f1cb209dd7a037fd008471bc5
VilMe <vilma.medrano00@gmail.com>
2026-01-05 22:40:08 -0600

    continue definig convert currency function

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_rates(mock: bool = False) -> dict:
 
 def get_currency(currency: str, rates: dict) -> float:
     currency: str = currency.upper()
-    if currency in rates.keys();
+    if currency in rates.keys():
         return rates.get(currency)
     else:
         raise ValueError(f'"{currency}" is not a valid currency. maybe you missed a letter or mixed a letter :)')
@@ -41,3 +41,8 @@ def get_currency(currency: str, rates: dict) -> float:
 
 def convert_currency(amount: float, base: str, vs: str, rates: dict) -> float:
     base_rate: float = get_currency(base, rates)
+    vs_rate: float = get_currency(vs, rates)
+
+
+    conversion: float = round((vs_rate / base_rate) * amount, 2)
+
