d1f3b5173c6e15b95e54295cb06bf7c773f0ae85
VilMe <vilma.medrano00@gmail.com>
2026-01-06 22:06:36 -0600

    uncomment api key and base url to make a live call

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 
 
 # Contstants
-# BASE_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
-# API_KEY: Final[str] = os.environ['EXCHANGE_RATE_API_KEY']
+BASE_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
+API_KEY: Final[str] = os.environ['EXCHANGE_RATE_API_KEY']
 
 
 
